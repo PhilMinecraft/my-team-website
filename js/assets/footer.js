3cf0905e0c6c4a743589e85d41edbacdf288cb7a
PhilMinecraft <arturo.rabino.3@outlook.com>
2022-02-05 17:26:22 +0800

    .

--- js/assets/footer.js
+++ js/assets/footer.js
@@ -1,10 +1,12 @@
 function openLanguage(){
     document.getElementById('current').onclick = closeLanguage;
+    document.getElementById('current').style.position = 'relative';
     document.getElementById('selection').style.display = 'flex';
     document.getElementById('language').style.display = 'block';
   }
   function closeLanguage(){
     document.getElementById('current').onclick = openLanguage;
+    document.getElementById('current').style.position = 'static';
     document.getElementById('selection').style.display = 'none';
     document.getElementById('language').style.display = 'none';
   }
