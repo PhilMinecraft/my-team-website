2c65ca41a62d3ab8c5a16cc18a25b9a96c56eac5
Qky <qkie0915@gmail.com>
2022-03-08 20:39:29 +0700

    what the fuck is this getting deleted?
    Co-authored-by: PhilMinecraft <arturo.rabino.3@outlook.com>

--- store/js/assets/header.js
+++ store/js/assets/header.js
@@ -9,3 +9,36 @@ function headerScroll() {
     document.getElementById("header").className = "header";
   }
 }
+
+
+function openLanguage() {
+  document.getElementById("current").onclick = closeLanguage;
+  document.getElementById("selection").style.display = "flex";
+  document.getElementById("language").style.display = "block";
+}
+function closeLanguage() {
+  document.getElementById("current").onclick = openLanguage;
+  document.getElementById("selection").style.display = "none";
+  document.getElementById("language").style.display = "none";
+}
+
+setTimeout(function () {
+  $("*[data-cr-date]").text(DtUv29x0() - 1 + " - " + DtUv29x0());
+  $(".current>img")
+    .attr(
+      "src",
+      `https://flagcdn.com/${
+        gq("lang").split("-")[1] ||
+        localStorage.getItem("language").split("-")[1] ||
+        "us"
+      }.svg`
+    )
+    .attr("alt", gq("lang") || "en-us");
+  $(".current>h6").text(
+    (
+      gq("lang").split("-")[1] ||
+      localStorage.getItem("language").split("-")[1] ||
+      "US"
+    ).toUpperCase()
+  );
+}, 500);
