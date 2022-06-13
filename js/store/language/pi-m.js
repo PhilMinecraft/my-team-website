542c53985f6a035dc26a40befa069904b7abad24
PhilMinecraft <arturo.rabino.3@outlook.com>
2022-06-13 20:29:48 +0800

    Add files via upload

---
+++ js/store/language/pi-m.js
@@ -0,0 +1,274 @@
+const packinfo = {
+    "en-gb" : {
+        "tags" : "TAGS:",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "GENRES:",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "PLAYERS:",
+
+        /* "players_i-" : "", */
+    },
+    "en-us" : {
+        "tags" : "TAGS:",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "GENRES:",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "PLAYERS:",
+
+        /* "players_i-" : "", */
+    },
+    "es-es" : {
+        "tags" : "ETIQUETAS:",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "GÉNEROS:",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "JUGADORES:",
+
+        /* "players_i-" : "", */
+    },
+    "fil-ph" : {
+        "tags" : "MGA TAG:",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "MGA GENRE:",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "MGA PLAYER:",
+
+        /* "players_i-" : "", */
+    },
+    "hi-in" : {
+        "tags" : "टैग:",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "शैलियों:",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "खिलाड़ियों:",
+
+        /* "players_i-" : "", */
+    },
+    "id-id" : {
+        "tags" : "TAGS:",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "GENRE:",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "PEMAIN:",
+
+        /* "players_i-" : "", */
+    },
+    "ja-jp" : {
+        "tags" : "タグ：",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "ジャンル：",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "選手：",
+
+        /* "players_i-" : "", */
+    },
+    "km-kh" : {
+        "tags" : "ស្លាក:",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "ប្រភេទ៖",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "អ្នកលេង៖",
+
+        /* "players_i-" : "", */
+    },
+    "ko-kr" : {
+        "tags" : "TAGS:",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "GENRES:",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "PLAYERS:",
+
+        /* "players_i-" : "", */
+    },
+    "ms-my" : {
+        "tags" : "TAGS:",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "GENRE:",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "PEMAIN:",
+
+        /* "players_i-" : "", */
+    },
+    "my-mm" : {
+        "tags" : "တက်ဂ်များ",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "အမျိုးအစားများ-",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "ကစားသမားများ-",
+
+        /* "players_i-" : "", */
+    },
+    "pt-pt" : {
+        "tags" : "ETIQUETAS:",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "GÉNEROS:",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "JOGADORES:",
+
+        /* "players_i-" : "", */
+    },
+    "ru-ru" : {
+        "tags" : "ТЕГИ:",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "ЖАНРОВ:",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "ИГРОКОВ:",
+
+        /* "players_i-" : "", */
+    },
+    "vi-vn" : {
+        "tags" : "TAGS:",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "THỂ LOẠI:",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "CẦU THỦ:",
+
+        /* "players_i-" : "", */
+    },
+    "zh-cn" : {
+        "tags" : "标签：",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "流派：",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "球员：",
+
+        /* "players_i-" : "", */
+    },
+    "zh-tw" : {
+        "tags" : "标签：",
+
+        "tags_i-Chinese" : "",
+        "tags_i-Pixel_Art" : "",
+        "tags_i-Stylized" : "",
+
+
+        "genres" : "流派：",
+
+        /* "genres_i-" : "", */
+
+
+        "players" : "球员：",
+
+        /* "players_i-" : "", */
+    }
+}
