da84ad4636435dfc5a81409dab8405b511d432cb
PhilMinecraft <arturo.rabino.3@outlook.com>
2022-06-16 14:59:50 +0800

    Add files via upload

---
+++ js/store/initialize.js
@@ -0,0 +1,84 @@
+"use strict";
+
+const languageContent = {
+  "en-gb": {
+    "pp-f": "FREE",
+    "pg": "Download",
+    "cl": "Link copied"
+  },
+  "en-us": {
+    "pp-f": "FREE",
+    "pg": "Download",
+    "cl": "Link copied"
+  },
+  "es-es": {
+    "pp-f": "GRATIS",
+    "pg": "Descargar",
+    "cl": "Enlace copiado"
+  },
+  "fil-ph": {
+    "pp-f": "LIBRE",
+    "pg": "I-download",
+    "cl": "Na-copy ang link"
+  },
+  "hi-in": {
+    "pp-f": "उचित",
+    "pg": "डाउनलोड",
+    "cl": "लिंक क्लिपबोर्ड में लग चुकी है"
+  },
+  "id-id": {
+    "pp-f": "GRATIS",
+    "pg": "Unduh",
+    "cl": "Tautan disalin"
+  },
+  "ja-jp": {
+    "pp-f": "無料",
+    "pg": "ダウンロード",
+    "cl": "コピーされたリンク"
+  },
+  "km-kh": {
+    "pp-f": "ಉಚಿತឥតគិតថ្លៃ",
+    "pg": "ទាញយក",
+    "cl": "តំណ ដែល បាន ចម្លង"
+  },
+  "ko-kr": {
+    "pp-f": "무료",
+    "pg": "다운로드",
+    "cl": "링크 복사됨"
+  },
+  "ms-my": {
+    "pp-f": "PERCUMA",
+    "pg": "Muat turun",
+    "cl": "Pautan disalin"
+  },
+  "my-mm": {
+    "pp-f": "လွတ်လပ်မှု",
+    "pg": "ဒေါင်းလုတ်ဆွဲပါ",
+    "cl": "လင့်ခ်ကူးယူထား"
+  },
+  "pt-pt": {
+    "pp-f": "GRÁTIS",
+    "pg": "Baixar",
+    "cl": "Ligação copiada"
+  },
+  "ru-ru": {
+    "pp-f": "СВОБОДНЫЙ",
+    "pg": "Загружать",
+    "cl": "Ссылка скопирована"
+  },
+  "vi-vn": {
+    "pp-f": "TỰ DO",
+    "pg": "Tải xuống",
+    "cl": "Nối kết được sao chép"
+  },
+  "zh-cn": {
+    "pp-f": "自由",
+    "pg": "下载",
+    "cl": "链接已复制"
+  },
+  "zh-tw": {
+    "pp-f": "自由",
+    "pg": "下载",
+    "cl": "链接已复制"
+  }
+};
